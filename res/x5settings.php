5dabb878572548c76058a2d8405a7cdb14b45e61
damnilociaramaglia <dancia43@gmail.com>
2026-02-21 18:24:19 +0100

    abave

---
+++ res/x5settings.php
@@ -0,0 +1,106 @@
+<?php
+
+/*
+|-------------------------------
+|	GENERAL SETTINGS
+|-------------------------------
+*/
+
+$imSettings['general'] = array(
+	'site_id' => '6F1F79AC4320061A2A338CFF44A51F99',
+	'url' => 'http://localhost/',
+	'homepage_url' => 'http://localhost/index.html',
+	'icon' => '',
+	'version' => '2025.1.7.0',
+	'sitename' => 'https://www.abave.netsons.org',
+	'lang_code' => 'it-IT',
+	'public_folder' => '',
+	'salt' => '0gxsozd681e44q3ahrwpnprt5iz2c0f392zja9',
+	'common_email_sender_addres' => 'danilo214@alice.it',
+	'enable_sender_header' => true,
+	'date_format' => 'dd MMM yyyy',
+	'date_format_ext' => 'dddd dd MMM yyyy',
+	'date_format_no_day' => 'MMM yyyy',
+	'date_format_no_day_ext' => 'MMM yyyy'
+);
+/*
+|-------------------------------
+|	BREAKPOINTS
+|-------------------------------
+*/
+
+$imSettings['breakpoints'] = array(
+	array("name" => "Desktop", "hash" => "64fc08783617d04d904ff96094f48dc9", "start" => "max", "end" => 1500.0, "fluid" => false),
+	array("name" => "Breakpoint 1", "hash" => "4f185bb78051b0b03abc8b37cf501f30", "start" => 1499.9, "end" => 900.0, "fluid" => false),
+	array("name" => "Mobile", "hash" => "44fdb9ec8cca1346727f8308cce22e5a", "start" => 899.9, "end" => 700.0, "fluid" => false),
+	array("name" => "Mobile Fluid", "hash" => "5ecdcca63de80fd3d4fbb36295d22b7d", "start" => 699.9, "end" => 0.0, "fluid" => true),
+);
+/*
+|-------------------------------
+|	PASSWORD POLICY
+|-------------------------------
+*/
+
+$imSettings['password_policy'] = array(
+	'required_policy' => false,
+	'minimum_characters' => '6',
+	'include_uppercase' => false,
+	'include_numeric' => false,
+	'include_special' => false
+);
+/*
+|-------------------------------
+|	Captcha
+|-------------------------------
+*/ImTopic::$captcha_code = "		<div class=\"x5captcha-wrap\">
+			<label for=\"3s2yfo3a-imCpt\">Parola di controllo:</label><br />
+			<input type=\"text\" id=\"3s2yfo3a-imCpt\" class=\"imCpt\" name=\"imCpt\" maxlength=\"5\" />
+		</div>
+";
+
+
+$imSettings['admin'] = array(
+	'icon' => 'admin/images/logo_g4uxnljv.png',
+	'theme' => 'orange',
+	'extra-dashboard' => array(),
+	'extra-links' => array()
+);
+
+
+/*
+|--------------------------------------------------------------------------------------
+|	DATABASES SETTINGS
+|--------------------------------------------------------------------------------------
+*/
+
+$imSettings['databases'] = array();
+
+/*
+|-------------------------------------------------------------------------------------------
+|	GUESTBOOK SETTINGS
+|-------------------------------------------------------------------------------------------
+*/
+
+$imSettings['guestbooks'] = array();
+
+
+/*
+|-------------------------------
+|	EMAIL SETTINGS
+|-------------------------------
+*/
+
+$ImMailer->emailType = 'html';
+$ImMailer->header = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">' . "\n" . '<html>' . "\n" . '<head>' . "\n" . '<meta http-equiv="content-type" content="text/html; charset=utf-8">' . "\n" . '<meta name="generator" content="Incomedia WebSite X5 Evolution 2025.1.7 - www.websitex5.com">' . "\n" . '</head>' . "\n" . '<body bgcolor="#C0C0C0" style="background-color: #C0C0C0;">' . "\n\t" . '<table border="0" cellpadding="0" align="center" cellspacing="0" style="padding: 0; margin: 0 auto; width: 700px; border-collapse: separate; ">' . "\n\t" . '<tr><td id="imEmailContent" style="min-height: 300px; padding: 10px; font: normal normal normal 8pt \'Tahoma\'; color: #000000; background-color: #FFFFFF; text-decoration: none; text-align: left; width: 700px; border-style: solid; border-color: #808080 #808080 #808080 #808080; border-top-width: 1px; border-right-width: 1px; border-bottom-width: 0; border-bottom: none; border-left-width: 1px;border-top-left-radius: 0px; border-top-right-radius: 0px;  margin-top: 25px;  margin-bottom: 25px;  margin-left: 25px;  margin-right: 25px;  background-color: #FFFFFF" width="700px">' . "\n\t\t";
+$ImMailer->footer = "\n\t" . '</td></tr>' . "\n\t" . '<tr><td id="imEmailIcons" style="background-color: #FFFFFF;border-left-style: solid; border-left-color: #808080; border-left-width: 1px; border-right-style: solid; border-right-color: #808080; border-right-width: 1px;  padding-top: 25px;  padding-bottom: 25px;  padding-left: 15px;  padding-right: 15px;  text-align: center;  min-height: 300px; " width="700"></td></tr>' . "\n\t" . '<tr><td id="imEmailFooter" style="font: normal normal normal 7pt \'Tahoma\'; color: #000000; background-color: transparent; text-decoration: none; text-align: center;  width: 700px;border-style: solid; border-color: #808080 #808080 #808080 #808080; border-top-width: 0; border-top: none; border-right-width: 1px; border-bottom-width: 1px; border-left-width: 1px; border-bottom-left-radius: 0px; border-bottom-right-radius: 0px; padding: 10px; background-color: #FFFFFF" width="700px">' . "\n\t\t" . 'Questo messaggio di posta elettronica contiene informazioni rivolte esclusivamente al destinatario sopra indicato.<br>Nel caso aveste ricevuto questo messaggio di posta elettronica per errore, siete pregati di segnalarlo immediatamente al mittente e distruggere quanto ricevuto senza farne copia.' . "\n\t" . '</td></tr>' . "\n\t" . '</table>' . "\n" . '</body>' . "\n" . '</html>';
+$ImMailer->bodyBackground = '#FFFFFF';
+$ImMailer->bodyBackgroundEven = '#FFFFFF';
+$ImMailer->bodyBackgroundOdd = '#F0F0F0';
+$ImMailer->bodyBackgroundBorder = '#CDCDCD';
+$ImMailer->bodyTextColorOdd = '#000000';
+$ImMailer->bodySeparatorBorderColor = '#000000';
+$ImMailer->emailBackground = '#C0C0C0';
+$ImMailer->emailContentStyle = 'font: normal normal normal 8pt \'Tahoma\'; color: #000000; background-color: #FFFFFF; text-decoration: none; text-align: left; ';
+$ImMailer->emailContentFontFamily = 'font-family: Tahoma;';
+
+// End of file x5settings.php
